117e10fb3efc12c158733c653769b1aacf504b1b
Ryusei Yamaguchi <mandel59@gmail.com>
2022-07-21 08:44:31 +0900

    Implement table and column names completion

--- bin/erq-cli.js
+++ bin/erq-cli.js
@@ -43,20 +43,253 @@ const dbpath = options.db ?? ":memory:";
 const db = new Database(dbpath);
 console.error("Connected to %s", dbpath);
 
+// global states
+
+/** @type {"read" | "eval"} */
+let state = "read";
+let input = "";
+let sigint = false;
+
+// autocompleter
+
+const keywords = new Set([
+  "ABORT",
+  "ACTION",
+  "ADD",
+  "AFTER",
+  "ALL",
+  "ALTER",
+  "ALWAYS",
+  "ANALYZE",
+  "AND",
+  "AS",
+  "ASC",
+  "ATTACH",
+  "AUTOINCREMENT",
+  "BEFORE",
+  "BEGIN",
+  "BETWEEN",
+  "BY",
+  "CASCADE",
+  "CASE",
+  "CAST",
+  "CHECK",
+  "COLLATE",
+  "COLUMN",
+  "COMMIT",
+  "CONFLICT",
+  "CONSTRAINT",
+  "CREATE",
+  "CROSS",
+  "CURRENT",
+  "CURRENT_DATE",
+  "CURRENT_TIME",
+  "CURRENT_TIMESTAMP",
+  "DATABASE",
+  "DEFAULT",
+  "DEFERRABLE",
+  "DEFERRED",
+  "DELETE",
+  "DESC",
+  "DETACH",
+  "DISTINCT",
+  "DO",
+  "DROP",
+  "EACH",
+  "ELSE",
+  "END",
+  "ESCAPE",
+  "EXCEPT",
+  "EXCLUDE",
+  "EXCLUSIVE",
+  "EXISTS",
+  "EXPLAIN",
+  "FAIL",
+  "FILTER",
+  "FIRST",
+  "FOLLOWING",
+  "FOR",
+  "FOREIGN",
+  "FROM",
+  "FULL",
+  "GENERATED",
+  "GLOB",
+  "GROUP",
+  "GROUPS",
+  "HAVING",
+  "IF",
+  "IGNORE",
+  "IMMEDIATE",
+  "IN",
+  "INDEX",
+  "INDEXED",
+  "INITIALLY",
+  "INNER",
+  "INSERT",
+  "INSTEAD",
+  "INTERSECT",
+  "INTO",
+  "IS",
+  "ISNULL",
+  "JOIN",
+  "KEY",
+  "LAST",
+  "LEFT",
+  "LIKE",
+  "LIMIT",
+  "MATCH",
+  "MATERIALIZED",
+  "NATURAL",
+  "NO",
+  "NOT",
+  "NOTHING",
+  "NOTNULL",
+  "NULL",
+  "NULLS",
+  "OF",
+  "OFFSET",
+  "ON",
+  "OR",
+  "ORDER",
+  "OTHERS",
+  "OUTER",
+  "OVER",
+  "PARTITION",
+  "PLAN",
+  "PRAGMA",
+  "PRECEDING",
+  "PRIMARY",
+  "QUERY",
+  "RAISE",
+  "RANGE",
+  "RECURSIVE",
+  "REFERENCES",
+  "REGEXP",
+  "REINDEX",
+  "RELEASE",
+  "RENAME",
+  "REPLACE",
+  "RESTRICT",
+  "RETURNING",
+  "RIGHT",
+  "ROLLBACK",
+  "ROW",
+  "ROWS",
+  "SAVEPOINT",
+  "SELECT",
+  "SET",
+  "TABLE",
+  "TEMP",
+  "TEMPORARY",
+  "THEN",
+  "TIES",
+  "TO",
+  "TRANSACTION",
+  "TRIGGER",
+  "UNBOUNDED",
+  "UNION",
+  "UNIQUE",
+  "UPDATE",
+  "USING",
+  "VACUUM",
+  "VALUES",
+  "VIEW",
+  "VIRTUAL",
+  "WHEN",
+  "WHERE",
+  "WINDOW",
+  "WITH",
+  "WITHOUT",
+]);
+
+const patName = "[\\p{Lu}\\p{Ll}\\p{Lt}\\p{Lm}\\p{Lo}\\p{Nl}][\\p{Lu}\\p{Ll}\\p{Lt}\\p{Lm}\\p{Lo}\\p{Nl}\\p{Mc}\\p{Nd}\\p{Pc}\\p{Cf}]*"
+const patQuot = "`[^`]*`"
+const patPart = "`[^`]*"
+const reName = new RegExp(`^${patName}$`, "u");
+const reFQNamePart = new RegExp(`(?:(?:${patName}|${patQuot})\\.){0,2}(?:${patName}|${patQuot}|${patPart})?$`, "u");
+const reParseColumnName = new RegExp(`^(${patName}|${patQuot})(?:\\.(${patName}|${patQuot}))?\\.(${patName}|${patQuot}|${patPart})?$`, "u");
+
+function quoteSQLName(name) {
+  if (keywords.has(name.toUpperCase())) {
+    return `\`${name}\``;
+  }
+  if (!reName.test(name)) {
+    return `\`${name.replace(/`/g, "``")}\``;
+  }
+  return name;
+}
+
+function unquoteSQLName(quot) {
+  if (quot[0] === "`") {
+    if (quot[quot.length - 1] === "`") {
+      return quot.substring(1, quot.length - 1).replace(/``/g, "`");
+    }
+    return quot.substring(1).replace(/``/g, "`");
+  }
+  return quot;
+}
+
+function getTables() {
+  /** @type {{schema: string, name: string, type: string, ncol: number, wr: 0 | 1, strict: 0 | 1}[]} */
+  const tables = db.prepare("pragma table_list").all();
+  return tables;
+}
+
+function getColumns(schema, table) {
+  /** @type {{cid: number, name: string, type: string, notnull: 0 | 1, dflt_value: any, pk: 0 | 1}[]} */
+  const columns = db.prepare(`pragma ${quoteSQLName(schema)}.table_info(${quoteSQLName(table)})`).all();
+  return columns;
+}
+
+function completer(line) {
+  const m = reFQNamePart.exec(line);
+  const tables = getTables();
+  const schemas = Array.from(new Set(tables.map(t => t.schema)).values());
+  const tableNamesFQ = tables.map(t => `${quoteSQLName(t.schema)}.${quoteSQLName(t.name)}`);
+  const tableNames = tables.map(t => quoteSQLName(t.name));
+  const q = m[0];
+  // column completion
+  {
+    const m = reParseColumnName.exec(q);
+    if (m != null) {
+      const m1 = unquoteSQLName(m[1]);
+      const m2 = m[2] && unquoteSQLName(m[2]);
+      const m3 = m[3] ? unquoteSQLName(m[3]) : "";
+      const [sn, tn] = (m2 != null) ? [m1, m2] : [tables.find(t => t.name === m1)?.schema, m1];
+      if (sn != null) {
+        const columns = getColumns(sn, tn).filter(c => c.name.startsWith(m3));
+        if (m2 != null) {
+          const qtn = `${quoteSQLName(sn)}.${quoteSQLName(tn)}`;
+          return [columns.map(c => `${qtn}.${c.name}`), q];
+        } else {
+          const qtn = quoteSQLName(tn);
+          return [columns.map(c => `${qtn}.${c.name}`), q];
+        }
+      }
+    }
+  }
+  // table name completion
+  {
+    const matches = [...schemas, ...tableNames, ...tableNamesFQ].filter(n => n.startsWith(q)).sort();
+    if (matches.length > 0) {
+      return [matches, q];
+    }
+  }
+  return [[], q];
+}
+
+// readline
+
 const rl = readline.createInterface({
   input: stdin,
   output: stderr,
+  completer,
   prompt: 'erq> ',
 });
 
 const isTTY = stdin.isTTY && stderr.isTTY;
 
 let outputStream = stdout;
-
-/** @type {"read" | "eval"} */
-let state = "read";
-let input = "";
-let sigint = false;
 
 function setPrompt() {
   if (input === "") {
