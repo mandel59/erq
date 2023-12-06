f6fb968e1a16c072581f822b75c2faf5bde65bbe
Ryusei Yamaguchi <mandel59@gmail.com>
2023-12-07 01:44:01 +0900

    Add iconv functions

--- src/user-functions.js
+++ src/user-functions.js
@@ -3,6 +3,7 @@ import { resolve as pathResolve, basename, dirname } from "node:path";
 import jsdom from "jsdom";
 import memoizedJsonHash from "@mandel59/memoized-json-hash";
 import { feature } from "topojson-client";
+import iconv from "iconv-lite";
 import { geomToGeoJSON } from "./geo/geom-to-geojson.js";
 
 /**
@@ -490,5 +491,21 @@ export function defineUserFunctions(defineFunction, defineTable, defineAggregate
         bbox: null,
       }
     }
+  })
+
+  defineFunction("iconv_decode", { deterministic: true }, function (buffer, encoding) {
+    if (buffer == null) return null;
+    if (encoding == null) return null;
+    if (!Buffer.isBuffer(buffer)) throw new TypeError("iconv_decode(buffer,encoding) buffer must be a blob");
+    if (typeof encoding !== "string") throw new TypeError("iconv_decode(buffer,encoding) encoding must be a string");
+    return iconv.decode(buffer, encoding);
+  })
+
+  defineFunction("iconv_encode", { deterministic: true }, function (str, encoding) {
+    if (str == null) return null;
+    if (encoding == null) return null;
+    if (typeof str !== "string") throw new TypeError("iconv_encode(str,encoding) str must be a string");
+    if (typeof encoding !== "string") throw new TypeError("iconv_encode(str,encoding) encoding must be a string");
+    return iconv.encode(str, encoding);
   })
 }
