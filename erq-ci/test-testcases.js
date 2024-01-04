f4acb4783570274728167b2f49da8abf7909a457
Ryusei Yamaguchi <mandel59@gmail.com>
2024-01-04 21:34:21 +0900

    Update ci test

--- erq-ci/test-testcases.js
+++ erq-ci/test-testcases.js
@@ -5,6 +5,7 @@ import { basename } from "node:path";
 import { spawn } from "node:child_process";
 import streamConsumers from "node:stream/consumers";
 import assert from "node:assert";
+import looksSame from "looks-same";
 
 const dirs = readdirSync("testcases");
 
@@ -15,22 +16,59 @@ for (const dir of dirs) {
     const erqBaseName = basename(file, '.erq');
     const outFile = files.find((f) => f.startsWith(`${erqBaseName}.out`));
     if (!outFile) continue;
-    test(`${dir}/${basename(file, '.erq')}`, async (t) => {
-      const erqInput = createReadStream(`testcases/${dir}/${file}`);
-      const erqOutputExpected = await readFile(`testcases/${dir}/${outFile}`);
-      const expected = { exitCode: 0, output: erqOutputExpected };
-      const erqProcess = spawn("erq", [], {
-        stdio: ["pipe", "pipe", "ignore"],
-        timeout: 10000,
+    if (outFile.endsWith('.png')) {
+      test(`${dir}/${basename(file, '.erq')}`, async (t) => {
+        const erqInput = createReadStream(`testcases/${dir}/${file}`);
+        const erqOutputExpected = await readFile(`testcases/${dir}/${outFile}`);
+        const erqProcess = spawn("erq", [], {
+          stdio: ["pipe", "pipe", "ignore"],
+          timeout: 10000,
+        });
+        erqInput.pipe(erqProcess.stdin);
+        const erqOutputPromise = streamConsumers.buffer(erqProcess.stdout);
+        const erqExitCode = await new Promise((resolve) => {
+          erqProcess.once("exit", (code) => { resolve(code); })
+        });
+        const erqOutputActual = await erqOutputPromise;
+        const { equal } = await looksSame(erqOutputActual, erqOutputExpected);
+        assert.ok(equal);
       });
-      erqInput.pipe(erqProcess.stdin);
-      const erqOutputPromise = streamConsumers.buffer(erqProcess.stdout);
-      const erqExitCode = await new Promise((resolve) => {
-        erqProcess.once("exit", (code) => { resolve(code); })
+    } else if (outFile.endsWith('.json')) {
+      test(`${dir}/${basename(file, '.erq')}`, async (t) => {
+        const erqInput = createReadStream(`testcases/${dir}/${file}`);
+        const erqOutputExpected = await readFile(`testcases/${dir}/${outFile}`);
+        const expected = { exitCode: 0, output: JSON.parse(erqOutputExpected.toString()) };
+        const erqProcess = spawn("erq", [], {
+          stdio: ["pipe", "pipe", "ignore"],
+          timeout: 10000,
+        });
+        erqInput.pipe(erqProcess.stdin);
+        const erqOutputPromise = streamConsumers.buffer(erqProcess.stdout);
+        const erqExitCode = await new Promise((resolve) => {
+          erqProcess.once("exit", (code) => { resolve(code); })
+        });
+        const erqOutputActual = await erqOutputPromise;
+        const actual = { exitCode: erqExitCode, output: JSON.parse(erqOutputActual.toString()) };
+        assert.deepEqual(actual, expected);
       });
-      const erqOutputActual = await erqOutputPromise;
-      const actual = { exitCode: erqExitCode, output: erqOutputActual };
-      assert.deepEqual(actual, expected);
-    });
+    } else {
+      test(`${dir}/${basename(file, '.erq')}`, async (t) => {
+        const erqInput = createReadStream(`testcases/${dir}/${file}`);
+        const erqOutputExpected = await readFile(`testcases/${dir}/${outFile}`);
+        const expected = { exitCode: 0, output: erqOutputExpected };
+        const erqProcess = spawn("erq", [], {
+          stdio: ["pipe", "pipe", "ignore"],
+          timeout: 10000,
+        });
+        erqInput.pipe(erqProcess.stdin);
+        const erqOutputPromise = streamConsumers.buffer(erqProcess.stdout);
+        const erqExitCode = await new Promise((resolve) => {
+          erqProcess.once("exit", (code) => { resolve(code); })
+        });
+        const erqOutputActual = await erqOutputPromise;
+        const actual = { exitCode: erqExitCode, output: erqOutputActual };
+        assert.deepEqual(actual, expected);
+      });
+    }
   }
 }
