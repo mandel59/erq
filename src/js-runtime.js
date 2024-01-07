babf3b27e7c5ec50c271cbadec401397a47b1848
Ryusei Yamaguchi <mandel59@gmail.com>
2024-01-07 20:06:22 +0900

    Fix memory leak error

--- src/js-runtime.js
+++ src/js-runtime.js
@@ -40,40 +40,55 @@ export class JSRuntime {
     if (!this.runtime) {
       throw new Error("Runtime not initialized");
     }
+
     const context = this.runtime.newContext();
-    try {
-      const argsHandle = context.newArray();
-      args.forEach((arg, i) => {
-        switch (typeof arg) {
-          case "string":
-            context.setProp(argsHandle, String(i), context.newString(arg));
-            break;
-          case "number":
-            context.setProp(argsHandle, String(i), context.newNumber(arg));
-            break;
-          case "boolean":
-            if (arg)
-              context.setProp(argsHandle, String(i), context.true);
-            else
-              context.setProp(argsHandle, String(i), context.false);
-            break;
-          case "object":
-            if (arg == null) {
-              context.setProp(argsHandle, String(i), context.null);
-            }
-            throw new Error("Unsupported argument type");
-          default:
-            throw new Error("Unsupported argument type");
-        }
-      })
-      context.setProp(context.global, "args", argsHandle);
-      const evalResult = context.evalCode(`import func from ${JSON.stringify(name)};`
-        + `globalThis.result = func.apply(null, globalThis.args);`);
-      context.unwrapResult(evalResult).dispose();
-      return context.getProp(context.global, "result").consume(context.dump);
-    } finally {
+
+    const argsHandle = context.newArray();
+    args.forEach((arg, i) => {
+      switch (typeof arg) {
+        case "string":
+          {
+            const handle = context.newString(arg);
+            context.setProp(argsHandle, String(i), handle);
+            handle.dispose();
+          }
+          break;
+        case "number":
+          {
+            const handle = context.newNumber(arg);
+            context.setProp(argsHandle, String(i), handle);
+            handle.dispose();
+          }
+          break;
+        case "boolean":
+          if (arg)
+            context.setProp(argsHandle, String(i), context.true);
+          else
+            context.setProp(argsHandle, String(i), context.false);
+          break;
+        case "object":
+          if (arg == null) {
+            context.setProp(argsHandle, String(i), context.null);
+          }
+          throw new Error("Unsupported argument type");
+        default:
+          throw new Error("Unsupported argument type");
+      }
+    })
+    context.setProp(context.global, "args", argsHandle);
+    argsHandle.dispose();
+
+    const evalResult = context.evalCode(`import func from ${JSON.stringify(name)};`
+      + `globalThis.result = func.apply(null, globalThis.args);`);
+    if (evalResult.error) {
+      const error = evalResult.error.consume(context.dump);
       context.dispose();
+      throw new Error(error);
     }
+    context.unwrapResult(evalResult).dispose();
+    const value = context.getProp(context.global, "result").consume(context.dump);
+    context.dispose();
+    return value;
   }
 }
 
