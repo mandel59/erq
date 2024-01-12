d6b74668974cc88d9023103289e15dfe89d73689
Ryusei Yamaguchi <mandel59@gmail.com>
2024-01-12 22:20:45 +0900

    Fix iconv module

--- src/modules/iconv.js
+++ src/modules/iconv.js
@@ -1,14 +1,14 @@
 import { createErqNodeJsModule } from "../create-erq-nodejs-module.js";
 
 export default createErqNodeJsModule('iconv', async ({ defineFunction }) => {
-  const { decode, encode } = await import("iconv-lite");
+  const { default: iconv } = await import("iconv-lite");
 
   defineFunction("decode", { deterministic: true }, function (buffer, encoding) {
     if (buffer == null) return null;
     if (encoding == null) return null;
     if (!Buffer.isBuffer(buffer)) throw new TypeError("iconv_decode(buffer,encoding) buffer must be a blob");
     if (typeof encoding !== "string") throw new TypeError("iconv_decode(buffer,encoding) encoding must be a string");
-    return decode(buffer, encoding);
+    return iconv.decode(buffer, encoding);
   })
 
   defineFunction("encode", { deterministic: true }, function (str, encoding) {
@@ -16,6 +16,6 @@ export default createErqNodeJsModule('iconv', async ({ defineFunction }) => {
     if (encoding == null) return null;
     if (typeof str !== "string") throw new TypeError("iconv_encode(str,encoding) str must be a string");
     if (typeof encoding !== "string") throw new TypeError("iconv_encode(str,encoding) encoding must be a string");
-    return encode(str, encoding);
+    return iconv.encode(str, encoding);
   })
 });
