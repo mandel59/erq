be46520cbf2901566dad264ed459c97090d092b6
Ryusei Yamaguchi <mandel59@gmail.com>
2025-09-11 19:10:43 +0900

    fix: move debug log behind ERQ_DEBUG flag

--- src/child.js
+++ src/child.js
@@ -530,7 +530,9 @@ export async function child() {
       if (ifNotExists && tableExists(table)) {
         return true;
       }
-      console.error(def)
+      if (DEBUG) {
+        console.error(def);
+      }
       const columnNames = def && def.columns.filter(c => !c.constraints.some(({ body }) => body.startsWith("as"))).map(c => c.name);
       let header, definition;
       if (def) {
